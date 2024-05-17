c164ecd029fa4caa9928593e8ea7e6c8b7975e82
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2024-05-17 16:00:00 +0530

    precision.py
    Today i learnt how to add precision to floats.

---
+++ precision.py
@@ -0,0 +1,4 @@
+# HOW TO SET PRECISION TO FLOAT DATA TYPE.
+result = 100/777
+print('the result is {r:.5f}'.format(r = result)) #this is used to set precision to a float data type.
+print(f'the result is {result:.5f}')#this is also a print format like .format but it is a newer version.
