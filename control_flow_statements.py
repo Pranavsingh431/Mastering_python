b259d8f0743f43945a644f469997185a915575cf
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2024-05-18 21:07:40 +0530

    control_flow_statements

---
+++ control_flow_statements.py
@@ -0,0 +1,14 @@
+# CONTROL FLOW STATEMENTS: if, else, elif.
+# colons and indentation(whitespaces) play a very important role in these statements.
+
+a = int(input("enter a number: "))
+b = int(input("enter another number: "))
+
+if a == 0 and b == 0:
+    print("Error because the numbers are zero.")
+elif a > b:
+    print(f'The number {a} is greater than {b}.')
+elif a < b:
+    print(f'The number {a} is less than {b}.') 
+else:
+    print('Both the numbers are equal.')   
