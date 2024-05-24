55c70f729c3976a7c26ffe3183422664f2011624
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2024-05-24 10:55:25 +0530

    for loop

---
+++ for_loop.py
@@ -0,0 +1,6 @@
+#FOR LOOP
+#iteration means running through each thing of an item whether it is string, number or a set block of code or a list or a dictionary.
+l = [1,2,3,4,5,6,7,8,9,10]
+for i in l:
+    if i%2 == 0: #to print the even numbers in the given list.
+        print(i)
