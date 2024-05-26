74e3102d4451e3eae0e52d1d22a6a614adabe452
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2024-05-26 14:42:58 +0530

    while loop

---
+++ while_loop.py
@@ -0,0 +1,10 @@
+#while loop continues to run a block of code until some condition remains True.
+#for example: while my dog is still hungry, keep giving him food.
+
+x = 0
+while x <= 5:
+    print(f'the current value of x is {x}')
+    x += 1
+    
+else:
+    print('now the value of x is greater than 5.') 
