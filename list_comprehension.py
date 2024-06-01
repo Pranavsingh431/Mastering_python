ef0ad581d74ec16f06c251c319116711742c8fd8
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2024-06-01 17:26:54 +0530

    list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,12 @@
+#LIST COMPREHENSION : using different ways to create lists.
+
+l = []
+for i in 'word':
+     l.append(i)
+     
+print(l)     
+
+####################
+#same thing as above but in just a single line.
+l  = [i for i in 'word']
+print(l)
