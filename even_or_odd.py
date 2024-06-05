6ac5e38c522c3f715b6c48c55a26c39b0b03cd78
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2024-06-05 13:37:21 +0530

    function to check whether a number is even or odd

---
+++ even_or_odd.py
@@ -0,0 +1,6 @@
+#to check whether a given number is even or odd.
+def even(num):
+    if num % 2 == 0:
+        return f"{num} is a even number."
+    else:
+        return f'{num} is an odd number.'
