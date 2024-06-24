ceedd45d4475a4f18f7d46f3f9f376bcb9f77ff7
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2024-06-24 15:10:17 +0530

    Create vector.py

---
+++ vector.py
@@ -0,0 +1,5 @@
+#simplest way to create vectors is as a list of numbers.
+height_weight_age = [173, #cm
+                     60,  #kg
+                     18]  #years   #this is a 3 dimensional vector as it has 3 axis.
+#similarly we can create any number of vectors with n dimensions very easily.
